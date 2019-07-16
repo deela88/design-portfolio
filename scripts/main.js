bea88e207a48ae2f5d1053e7ae5511d2794ab6ae
Deanna <lam.deanna@gmail.com>
2019-07-15 22:50:14 -0700

    Updated portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -86,3 +86,18 @@ var donutChart2 = new Chart(CHART4, {
   }
 }
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
